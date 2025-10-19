a5c34558e67b34ea84f2a72101be7e85a92770af
Cteklitz <cteklitz@ufl.edu>
2025-10-19 15:12:27 -0400

    Start implementing msgs, something doesnt work rn
    idk what is wrong but it does not work rn zz

--- peerProcess.py
+++ peerProcess.py
@@ -3,6 +3,7 @@ import sys
 import socket
 import threading
 import time
+import random
 
 peers = []
 peer_id = 0
@@ -20,6 +21,9 @@ class Peer:
         self.bitfield = bitfield # this peers bitfield
         self.preferred = False # whether this peer is a preferred neightbor
         self.optimistic = False # whether this peer is the optimistically unchoked neighbor
+        self.unchoked = False # whether this peer has unchoked self (is this peer sending us data)
+        self.outstanding_request = False # whether there is a current request that has not been replied to
+        self.requested = -1 # the currently requested piece, -1 if none
         # TODO: add fields for data rate from peer
 
 def getPrefCount(): # returns the amount of neighbors currently prefered
@@ -34,6 +38,15 @@ def getPrefNeighbors(): # returns an array of the current prefered neighbors
     for peer in peers:
         if peer.preferred:
             out.append(peer)
+    return out
+
+def getPrefNeighborsString():
+    out = ""
+    pref = getPrefNeighbors()
+    for peer in pref:
+        out += str(peer.id)
+        out += ","
+    out = out[0:len(out) - 1] # remove trailing comma
     return out
 
 def getOptimistic(): # returns the current optimistic unchoked peer, None if there is not one currently
@@ -69,7 +82,33 @@ def decodeBitfield(string): # returns a bitfield array for input bitfield hex st
         else:
             bitfield.append(False)
     return bitfield
+
+def bitfieldHasCount(bitfield): # returns the amount of pieces present in a bitfield
+    count = 0
+    for bit in bitfield:
+        if bit:
+            count += 1
+    return count
+
+def getRandomNeededIndex(): # returns the index of a random bit self needs
+    needed = []
+    for i in range(len(self.bitfield)):
+        if not self.bitfield[i]:
+            needed.append(i)
+    rand = random.randint(0, len(needed) - 1)
+    return needed[rand]
     
+
+def checkBitField(bitfield): # returns True if the input bitfield has any pieces that self does not have, False otherwise
+    for i in range(len(bitfield)):
+        if not self.bitfield[i] and bitfield[i]:
+            return True
+    return False
+
+def intToHex(num, len):
+    hex_num = hex(num)[2:]
+    hex_num = hex_num.zfill(len) # pad msg with 0s 
+    return hex_num
 
 def parsePeerInfo(): # returns an array of Peer object containing the data from PeerInfo.cfg
     cfg = open("PeerInfo.cfg", "r")
@@ -168,8 +207,7 @@ def connection(_peer_id):
 
     # send bitfield msg
     bitfield_string = encodeBitfield(self.bitfield)
-    bitfield_msg_len = (hex(len(bitfield_string) + 1))
-    bitfield_msg_len = bitfield_msg_len.zfill(4) # pad msg with 0s 
+    bitfield_msg_len = intToHex(len(bitfield_string) + 1, 4)
     bitfield_msg = str(bitfield_msg_len)
 
     bitfield_msg += "5"
@@ -183,13 +221,93 @@ def connection(_peer_id):
         print(f"Error: expected msg type 5, received: {t}")
         return
     connected_peer.bitfield = decodeBitfield(bitfield_string)
+
+    sending_thread = threading.Thread(target=sending, args=(_peer_id,))
+    receiving_thread = threading.Thread(target=receiving, args=(_peer_id,))
+    sending_thread.start()
+    receiving_thread.start()
+    sending_thread.join()
+    receiving_thread.join()
     
     connected_peer.connection.close() # temporary
+
+def sending(_peer_id): # loop to send msgs to a peer
+    connected_peer = getPeer(_peer_id)
+    s = connected_peer.connection
+    s.settimeout(10.0)
+    while True: # change to be while this peer does not have full file
+        #print(f"{connected_peer.id}: {connected_peer.unchoked}")
+        if connected_peer.unchoked and not connected_peer.outstanding_request and not self.has_file:
+            # send request msg
+            connected_peer.outstanding_request = True
+            index = getRandomNeededIndex()
+            msg = "00056" + intToHex(index, 4)
+            #print(f"requesting: {index}")
+            s.send(msg.encode())           
+        elif checkBitField(connected_peer.bitfield) and not connected_peer.unchoked and not self.has_file:
+            # send interested msg
+            msg = "00012"
+            s.send(msg.encode())
+            time.sleep(2)
+
+        if connected_peer.preferred or connected_peer.optimistic:
+            if connected_peer.requested != -1:
+                # send piece
+                #print(f"sending: {connected_peer.requested}")
+                msg = "0005" + "7" + intToHex(connected_peer.requested, 4) # TODO: Add data to msg
+                s.send(msg.encode())
+                connected_peer.requested = -1
+
+
+def receiving(_peer_id): # loop to receive msgs from a peer
+    connected_peer = getPeer(_peer_id)
+    s = connected_peer.connection
+
+    s.settimeout(10.0)
+    timeouts = 0
+
+    while timeouts < 15: # change to end loop once all peers are connected eventually, based on timeout for testing
+        try:
+            t, payload = reciveMessage(connected_peer.connection)
+            #print(f"{t}: {payload}")
+
+            if t == 0: # choke
+                connected_peer.unchoked = False
+                log(f"Peer {peer_id} is choked by {connected_peer.id}.")
+            elif t == 1: # unchoke
+                if not connected_peer.unchoked:
+                    connected_peer.unchoked = True
+                    log(f"Peer {peer_id} is unchoked by {connected_peer.id}.")
+            elif t == 2: # interested
+                log(f"Peer {peer_id} received the 'interested' message from {connected_peer.id}.")
+                if getPrefCount() < num_pref_neighbors: # TODO: propper choking/unchoking
+                    if not connected_peer.preferred:
+                        connected_peer.preferred = True
+                        msg = "00011"
+                        s.send(msg.encode())
+                        log(f"Peer {peer_id} has the preferred neighbors {getPrefNeighborsString()}.")
+            elif t == 6: # request
+                connected_peer.requested = int(payload, 16)
+                #print(f"recived request: {connected_peer.requested}")
+            elif t == 7: # piece
+                index = int(payload[0:4], 16)
+                self.bitfield[index] = True
+                log(f"Peer {peer_id} has downloaded the piece {index} from {connected_peer.id}. Now the number of pieces it has is {bitfieldHasCount(self.bitfield)}.")
+                connected_peer.outstanding_request = False
+                if bitfieldHasCount(self.bitfield) == int(file_size/piece_size):
+                    self.has_file = True
+                    return
+                    # TODO: handle stuff for self having full file
+                # TODO: process data, write to file
+        except socket.timeout:
+            timeouts += 1
+
 
 def reciveMessage(socket): # recives a msg, returns a tuple of the type and payload
     msg_len = socket.recv(4).decode() # get msg len
     length = int(msg_len, 16)
     msg = socket.recv(length).decode() # get msg
+    #print(msg)
     type = int(msg[0])
     payload = msg[1:]
     return (type,payload)
