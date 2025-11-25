2a012856791b193d7b622a57bcfcd425e67a7c57
Cteklitz <cteklitz@ufl.edu>
2025-11-25 16:51:21 -0500

    Add delay to test script
    ensures peers are started in the correct order

--- test.ps1
+++ test.ps1
@@ -1,9 +1,17 @@
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1001"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1002"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1003"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1004"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1005"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1006"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1007"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1008"
+Start-Sleep -Milliseconds 500
 Start-Process powershell "-NoExit -Command python C:\Users\ctekl\P2P-PROJECT\peerProcess.py 1009"
